a4d4bbc961b470d8ea103af6738f6bc0c1a9aaca
Juvan Thavalingam <thavajuv@students.zhaw.ch>
2024-04-02 23:07:04 +0200

    add pause/play button

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -43,4 +43,14 @@ public class Planet : MonoBehaviour
         rb.position += velocity;
     }
 
+    public void pauseGame()
+    {
+        Time.timeScale = 0;
+    }
+
+    public void resumeGame()
+    {
+        Time.timeScale = 1;
+    }
+
 }
---
+++ Assets/Scripts/GameStateScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameStateScript : MonoBehaviour
+{
+    private bool isPaused = false;
+
+    public void pauseResumeGame()
+    {
+        if (!isPaused)
+        {
+            Time.timeScale = 0;
+            isPaused = true;
+        } else
+        {
+            Time.timeScale = 1;
+            isPaused = false;
+        }
+        
+    }
+
+   
+}
