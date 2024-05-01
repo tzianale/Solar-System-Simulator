6b680fe07b14c332d3b5aa757b0b55d6352d6222
waltefab <waltefab@students.zhaw.ch>
2024-05-01 09:51:04 +0200

    Update CelestialBody.cs

--- Assets/Scripts/CelestialBody.cs
+++ Assets/Scripts/CelestialBody.cs
@@ -7,13 +7,13 @@ public class CelestialBody : MonoBehaviour
 {
     public enum CelestialBodyType { Sun, Planet, Moon };
     Rigidbody rb;
-    public CelestialBodyType celestType;
-    public Vector3 velocity;
-    public float mass;
-    public bool isGravityOn;
-    public int day;
-    public float orbitRadius;
-    public float ratioToEarthYear = 1;
+    private CelestialBodyType celestType;
+    private Vector3 velocity;
+    private float mass;
+    private bool isGravityOn;
+    private int day;
+    private float orbitRadius;
+    private float ratioToEarthYear = 1;
     private CelestialBody[] celestialBodies;
 
     // Start is called before the first frame update
@@ -65,5 +65,25 @@ public class CelestialBody : MonoBehaviour
         float z = (float) Math.Sin(2 * Math.PI / (365.256363004 * ratioToEarthYear) * day) * orbitRadius;
         rb.MovePosition(new Vector3(x, 0, z));
     }
+    
+// Getter-Methoden
+    // Getter- und Setter-Methoden
+    public CelestialBodyType GetCelestialBodyType() => celestType;
+    public void SetCelestialBodyType(CelestialBodyType type) => celestType = type;
+
+    public float GetMass() => mass;
+    public void SetMass(float value) => mass = value;
+
+    public bool IsGravityEnabled() => isGravityOn;
+    public void SetGravityEnabled(bool enabled) => isGravityOn = enabled;
+
+    public int GetDayOfYear() => day;
+    public void SetDayOfYear(int dayOfYear) => day = dayOfYear;
+
+    public float GetOrbitRadius() => orbitRadius;
+    public void SetOrbitRadius(float radius) => orbitRadius = radius;
+
+    public float GetRatioToEarthYear() => ratioToEarthYear;
+    public void SetRatioToEarthYear(float ratio) => ratioToEarthYear = ratio;
 
 }
