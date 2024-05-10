05ae11fcf6765031bf2542c5020739d8e82930a2
Riccardo-Maffei <126154411+Riccardo-Maffei@users.noreply.github.com>
2024-05-10 09:08:47 +0200

    Refactored some getter / setter methods in PlanetListAnimator.cs, renamed PlanetListAnimator.cs class to match with file name, improved Documentation in PlanetListAnimator.cs

--- Assets/Scripts/PlanetListAnimator.cs
+++ Assets/Scripts/PlanetListAnimator.cs
@@ -2,7 +2,11 @@
 using UnityEngine;
 using System.Collections;
 
-public class PlanetListContainerScript : MonoBehaviour
+
+/// <summary>
+/// Handles the Planet List animation on button click
+/// </summary>
+public class PlanetListAnimator : MonoBehaviour
 {
     private Transform _planetListContainerTransform;
     private bool _listIsOpen;
@@ -13,55 +17,71 @@ public class PlanetListContainerScript : MonoBehaviour
     [SerializeField]
     private float moveSpeed = 1.0f;
     
+    
     public TextMeshProUGUI buttonText;
 
-    
+    /// <summary>
+    /// Getter / Setter methods for the Transform that will contain the Planet List Items
+    /// </summary>
+    public Transform PlanetListContainerTransform { private get; set; }
+
+    /// <summary>
+    /// Getter / Setter methods to read the List Is Open boolean outside of this Class
+    /// </summary>
+    public bool ListIsOpen { get; private set; }
+
+    /// <summary>
+    /// Called on Object Instantiation, sets the PlanetListContainerTransform to the Transform of the current Object
+    /// </summary>
     private void Start()
     {
-        _planetListContainerTransform = transform;
+        PlanetListContainerTransform = transform;
     }
 
-    public Transform PlanetListContainerTransform
-    {
-        get => _planetListContainerTransform;
-        set => _planetListContainerTransform = value;
-    }
-
-    public bool ListIsOpen
-    {
-        get => _listIsOpen;
-        private set => _listIsOpen = value;
-    }
-
+    /// <summary>
+    /// Called on button click, starts a new Coroutine where the list will be moved to the new position
+    /// </summary>
     public void OnArrowClick()
     {
         Debug.Log("Arrow clicked");
-        if (_listIsOpen)
+        if (ListIsOpen)
         {
             StartCoroutine(MovePanel(moveDistance));
-            _listIsOpen = false;
+            ListIsOpen = false;
             buttonText.text = "↑";
         } else {
             StartCoroutine(MovePanel(-moveDistance));
-            _listIsOpen = true;
+            ListIsOpen = true;
             buttonText.text = "↓";
         }
     }
 
-    private IEnumerator MovePanel(Vector3 target)
+    /// <summary>
+    /// Creates a nice moving animation for the Planet List Container using multi threading
+    /// </summary>
+    /// 
+    /// <param name="vectorToTarget">
+    /// The vector pointing towards the target.
+    /// Vector length should be equal to the distance between the points
+    /// </param>
+    /// 
+    /// <returns>
+    /// An IEnumerator, which allows for this method to be called in parallel
+    /// </returns>
+    private IEnumerator MovePanel(Vector3 vectorToTarget)
     {
-        var startPosition = _planetListContainerTransform.position;
-        var endPosition = startPosition + target;
+        var startPosition = PlanetListContainerTransform.position;
+        var endPosition = startPosition + vectorToTarget;
 
         var currentTime = 0.0f;
 
         while (currentTime < 1)
         {
             currentTime += Time.deltaTime * moveSpeed;
-            _planetListContainerTransform.position = Vector3.Lerp(startPosition, endPosition, currentTime);
+            PlanetListContainerTransform.position = Vector3.Lerp(startPosition, endPosition, currentTime);
             yield return null;
         }
 
-        _planetListContainerTransform.position = endPosition;
+        PlanetListContainerTransform.position = endPosition;
     }
 }
