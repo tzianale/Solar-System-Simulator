6a988cdcfc28bc9f499f6eda3dd8b63fcba51fe0
Juvan Thavalingam <thavajuv@students.zhaw.ch>
2024-06-02 17:30:17 +0200

    add date change panel

--- Assets/Scripts/Models/DateChange.cs
+++ Assets/Scripts/Models/DateChange.cs
@@ -1,26 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DateChange : MonoBehaviour
 {
-
+    // year 0 to 9999
+    public TMP_Dropdown dayDropdown;
     public TMP_Dropdown monthDropdown;
+    public InputField yearInputField;
     //private List<Resolution> filteredResolutions = new List<Resolution>(); // List to keep unique resolutions
 
     // Start is called before the first frame update
+
+    public GameObject datePanel;
+       
     void Start()
     {
-        PopulateResolutions();
+        PopulateMonth();
+        //CameraControlV2.
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        int year = PopulateYearRes();
+        int month = monthDropdown.GetComponent<TMP_Dropdown>().value + 1;
+
+        PopulateDayRes(year, month);
     }
-    void PopulateResolutions()
+    void PopulateMonth()
     {
         monthDropdown.ClearOptions();
         List<string> options = new List<string>();
@@ -41,5 +52,56 @@ public class DateChange : MonoBehaviour
 
 
         monthDropdown.AddOptions(options);
+    }
+
+    void PopulateDayRes(int year, int month)
+    {
+        dayDropdown.ClearOptions();
+
+        List<String> options = new List<String>();
+        options.Clear();
+
+        int days = DateTime.DaysInMonth(year, month);
+
+        for (int i = 1; i <= days; i++)
+        {
+            options.Add(i.ToString());
+        }
+
+        dayDropdown.AddOptions(options);
+    }
+
+    int PopulateYearRes()
+    {
+        String value = yearInputField.GetComponent<InputField>().text;
+
+        int year;
+        if (int.TryParse(value, out year))
+        {
+            if (year <= 0)
+            {
+                year = 0;
+                return 0;
+            }
+            else if (year >= 9999)
+            {
+                year = 9999;
+                return 9999;
+            } else if(year > 0 || year < 9999)
+            {
+                return year;
+            }
+        }
+        else
+        {
+            Debug.Log("Bitte geben Sie eine gültige Zahl ein.");
+        }
+
+        return 2024;
+    }
+
+    public void closePanel()
+    {
+        datePanel.SetActive(false);
     }
 }
