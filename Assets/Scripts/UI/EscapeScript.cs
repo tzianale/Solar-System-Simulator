ddc70ef2e0e6e1f228ce7594e9c43c40c2df5998
Riccardo-Maffei <126154411+Riccardo-Maffei@users.noreply.github.com>
2024-06-14 23:21:46 +0200

    Refactored EscapeScript and EscapeScriptTest to improve names of variables and documentation

--- Assets/Scripts/UI/EscapeScript.cs
+++ Assets/Scripts/UI/EscapeScript.cs
@@ -3,17 +3,29 @@ using UnityEngine.SceneManagement;
 
 namespace UI
 {
+    /// <summary>
+    /// Offers useful public methods to buttons.
+    /// When the buttons are clicked, a previously given GameObject will be hidden or activated.
+    /// There is also a third case, where, if the specific method is called, the game will switch to the Main Menu
+    /// </summary>
     public class EscapeScript : MonoBehaviour
     {
+        [SerializeField]
+        private GameObject panel;
 
-        public GameObject panel;
+        private const int MainMenuSceneId = 0;
+        
+        public GameObject Panel { private get; set; }
 
-        public GameObject Panel
+        private void Start()
         {
-            get { return panel; }
-            set { panel = value; }
+            Panel = panel;
         }
-        public void openPanel()
+
+        /// <summary>
+        /// Sets the panel to active, allowing for it to be seen in the scene
+        /// </summary>
+        public void OpenPanel()
         {
             if (Panel != null)
             {
@@ -21,7 +33,10 @@ namespace UI
             }
         }
 
-        public void closePanel()
+        /// <summary>
+        /// Sets the panel to inactive, effectively hiding it
+        /// </summary>
+        public void ClosePanel()
         {
             if (Panel != null)
             {
@@ -29,9 +44,12 @@ namespace UI
             }
         }
 
-        public void escapeGame()
+        /// <summary>
+        /// Loads the Main Menu scene
+        /// </summary>
+        public void EscapeGame()
         {
-            SceneManager.LoadSceneAsync(0);
+            SceneManager.LoadSceneAsync(MainMenuSceneId);
         }
     }
 }
