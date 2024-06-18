a592b7112adfe1d882c35f14ff9bf2d0af359fa8
Juvan Thavalingam <thavajuv@students.zhaw.ch>
2024-06-18 21:28:56 +0200

    add documentary models

--- Assets/Scripts/Models/PlanetListManager.cs
+++ Assets/Scripts/Models/PlanetListManager.cs
@@ -16,18 +16,12 @@ namespace Models
     /// </summary>
     public class PlanetListManager : MonoBehaviour
     {
-        /// <summary>
-        /// Stores the relevant indexes of the Planet Properties that are saved in the Planet Properties csv file
-        /// </summary>
         private enum DataPropertyIndexes
         {
             PlanetName = 0,
             PlanetType = 2
         }
-        
-        /// <summary>
-        /// Stores the relevant indexes of the Planet Descriptions that are saved in the Planet Descriptions csv file
-        /// </summary>
+
         private enum DataDescriptionIndexes
         {
             PlanetName,
@@ -90,11 +84,7 @@ namespace Models
 
         private bool _highlightedPlanetOriginalEmissionEnabled;
         private Color _highlightedPlanetOriginalEmissionValue;
-        
-        /// <summary>
-        /// Called on Script initialization, reads the data stored in the csv file and initialises List Elements and
-        /// Info Tabs accordingly
-        /// </summary>
+
         private void Start()
         {
             _highlightedPlanet.AddOnSetValueAction(HandlePlanetHighlighting);
@@ -128,14 +118,6 @@ namespace Models
                 }
             }
         }
-
-        /// <summary>
-        /// Turns off the highlighting effect on a deselected planet (oldPlanet) while turning it on for the newly
-        /// selected one (newPlanet). Both values can be null without exceptions being thrown
-        /// </summary>
-        /// 
-        /// <param name="oldPlanet">The planet that was highlighted previously (can be null)</param>
-        /// <param name="newPlanet">The planet to highlight (can be null)</param>
         private void HandlePlanetHighlighting(GameObject oldPlanet, GameObject newPlanet)
         {
             if (oldPlanet)
@@ -163,39 +145,6 @@ namespace Models
             }
         }
 
-        /// <summary>
-        /// Adds a new Planet to the Planet List, as well as creating the corresponding info tab
-        /// </summary>
-        /// 
-        /// <param name="planetSprite">
-        /// The Image that will be associated to this planet
-        /// </param>
-        /// 
-        /// <param name="planetName">
-        /// The name of this planet
-        /// </param>
-        /// 
-        /// <param name="planetObject">
-        /// The GameObject that represents the planet in the simulation
-        /// </param>
-        /// 
-        /// <param name="variableProperties">
-        /// The properties of a planet which will impact the simulation when (and if) changed
-        /// </param>
-        /// 
-        /// <param name="staticProperties">
-        /// The "fixed" properties of a planet, aka the ones that won't have to be changed dynamically.
-        /// Example: Planet Surface Temperature
-        /// </param>
-        /// 
-        /// <param name="liveStats">
-        /// The "live" properties of a planet, aka the ones that will have to be changed dynamically.
-        /// Example: Current Planet Speed
-        /// </param>
-        /// 
-        /// <param name="planetDescription">
-        /// A description about this planet
-        /// </param>
         private void CreateNewPlanet(Sprite planetSprite, string planetName, GameObject planetObject, 
             Dictionary<string, TwoObjectContainer<Func<string>, UnityAction<string>>> variableProperties,
             Dictionary<string, string> staticProperties, 
@@ -233,20 +182,7 @@ namespace Models
             
             planetInfoTab.SetActive(false);
         }
-        
-        /// <summary>
-        /// Sets up click behavior for a given GameObject and its children,
-        /// by assigning a list of actions to be executed on click
-        /// </summary>
-        /// 
-        /// <param name="planetObject">
-        /// The GameObject to which the click behavior will be assigned
-        /// </param>
-        /// 
-        /// <param name="actions">
-        /// A list of actions to be executed when the GameObject or its children are clicked.
-        /// Each action takes an integer parameter representing the click count
-        /// </param>
+
         private static void SetGameObjectOnClickBehaviour(GameObject planetObject, List<Action<int>> actions)
         {
             var onClick = planetObject.AddComponent<OnGameObjectClick>();
@@ -263,18 +199,6 @@ namespace Models
             }
         }
 
-        /// <summary>
-        /// Takes the properties data that has been loaded from the memory and transforms it in an easily readable,
-        /// planet separated list of Dictionaries
-        /// </summary>
-        /// 
-        /// <param name="data">
-        /// The "raw data" that has been loaded from the memory
-        /// </param>
-        /// 
-        /// <returns>
-        /// The refined list of Dictionaries - One dictionary for each body
-        /// </returns>
         private List<Dictionary<string, string>> UnpackPlanetDataFromCsv(List<List<string>> data)
         {
             var result = new List<Dictionary<string, string>>();
@@ -315,16 +239,6 @@ namespace Models
             return result;
         }
 
-        /// <summary>
-        /// Extracts planet descriptions from a CSV data structure, currently excluding moons and dwarf planets
-        /// </summary>
-        /// 
-        /// <param name="data">
-        /// A list of lists, where each inner list represents a row in the CSV
-        /// and each element in the inner list represents a cell in that row
-        /// </param>
-        /// 
-        /// <returns>The list of planet descriptions.</returns>
         private List<string> UnpackPlanetDescriptionsFromCsv(List<List<string>> data)
         {
             var planetDescriptions = new List<string>();
@@ -346,24 +260,6 @@ namespace Models
             return planetDescriptions;
         }
 
-
-        /// <summary>
-        /// Checks, depending on the given parameters, if the item from the csv file should be included
-        /// in the planet list and info tabs
-        /// </summary>
-        /// 
-        /// <param name="planetName">
-        /// The name of the planet
-        /// </param>
-        /// 
-        /// <param name="planetType">
-        /// The type of the planet
-        /// </param>
-        /// 
-        /// <returns>
-        /// A boolean, excluding moon and dwarf planet data from the planet list.
-        /// An exception is made for the earths moon when the simulation is in the sandbox mode
-        /// </returns>
         private bool IncludePlanetInfoInList(string planetName, string planetType)
         {
             var isMoonOrDwarf = planetType.Equals(MoonDetector) || planetType.Equals(DwarfDetector);
--- Assets/Scripts/Models/SettingScript.cs
+++ Assets/Scripts/Models/SettingScript.cs
@@ -3,8 +3,14 @@ using UnityEngine.SceneManagement;
 
 namespace Models
 {
+    /// <summary>
+    /// Provides functionality for loading scenes in the game.
+    /// </summary>
     public class SettingScript : MonoBehaviour
     {
+        /// <summary>
+        /// Provides functionality for loading scenes in the game.
+        /// </summary>
         public void loadScene()
         {
             SceneManager.LoadSceneAsync(0);
